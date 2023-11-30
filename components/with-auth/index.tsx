fe9b063792390544b6849db0940a420f0597c61b
Hamidreza Ghanbari <hamidrezaghanbari13@gmail.com>
2023-11-30 09:41:08 +0330

    chore(auth): add loading state

--- components/with-auth/index.tsx
+++ components/with-auth/index.tsx
@@ -1,16 +1,19 @@
 import { useRouter } from "next/router";
-import { useEffect, ReactNode } from "react";
+import { useEffect, ReactNode, useState } from "react";
 
 const WithAuth = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     if (!sessionStorage.getItem("token")) {
       router.replace("/login");
     }
+
+    setIsLoaded(true);
   }, []);
 
-  return <>{children}</>;
+  return <>{isLoaded ? children: <div className="w-screen h-screen flex items-center justify-center animate-pulse">isLoading...</div>}</>;
 };
 
 export default WithAuth;
